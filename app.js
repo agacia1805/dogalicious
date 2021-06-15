2d10f3e524c76f2994231e8ef5b5363c73e22689
agata1805 <agacia1805@gmail.com>
2021-06-15 13:33:49 +0200

    Add a getRandomDog function and make it usable

--- app.js
+++ app.js
@@ -19,10 +19,30 @@ const iconValue = {
     English_cocker_spaniel: 86,
     French_bulldog: 113,
     German_shepherd: 115,
+    Greyhound: 127,
+    Husky: 226,
+    Japanese_chin: 140,
+    Kurzhaar: 116,
+    Malamute: 9,
+    Miniature_Schnauzer: 168,
+    Newfoundland: 171,
+    Pharaoh_hound: 188,
+    Pomeranian: 193,
+    Poodle: 197,
+    Pug: 201,
+    Rottweiler: 210,
+    Saluki: 213,
+    Shar_pei: 79,
+    Sheltie: 221,
+    Shih_tzu: 223,
+    Springer_spaniel: 103,
+    St_bernard: 212,
+    Tibetan_mastiff: 244,
+    Yorkshire_terrier: 264,
 };
 
 function fetchDogApi(dogKey, dogId) {
- dogId = '50';
+ // dogId = '50';
 
  var myHeaders = new Headers();
  myHeaders.append("Content-Type", "application/json");
@@ -36,7 +56,7 @@ function fetchDogApi(dogKey, dogId) {
  fetch(`https://api.thedogapi.com/v1/breeds/${dogId}`, requestOptions)
    .then(response => response.json())
    .then(data => {
-    console.log(data)
+    console.log(data);
    let breedName =  data.name;
    let temperament = data.temperament;
    let dogWeight =  data.weight.metric;
@@ -61,79 +81,63 @@ function fetchDogApi(dogKey, dogId) {
 fetchDogApi();
 
 
-// function getRandomDog() {
-//     const ids = iconValue.value;
-//     const keys = Object.keys(ids);
-//     const randomIndex = Math.floor(Math.random() * keys.length);
-//
-// }
-//
-// getRandomDog();
+function getRandomDog(dogId) {
 
-document.ready(function(){
-    let dogId = '2';
+            const keys = 264;
+            let randomIndex = Math.floor(Math.random() * keys);
+            console.log(randomIndex);
+            fetchDogApi(dogKey, randomIndex);
 
-    $.getJSON(`https://api.thedogapi.com/v1/breeds/${dogId}`,
-        {
-            tags: keyword,
-            tagmode: "any",
-            format: "json"
-        },
-        function(data) {
-            var rnd = Math.floor(Math.random() * data.items.length);
+}
 
-            var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
 
-            $('body').css('background-image', "url('" + image_src + "')");
+document.getElementById('searchRandom').addEventListener('click', getRandomDog);
 
-        });
-
-});
 
 function getICON(icon) {
  switch (icon) {
      case iconValue.Afghan_hound:
-         return "/img/dog breeds/afghan-hound.png";
+         return "img/dog breeds/afghan-hound.png";
      case iconValue.Airedale:
-         return "/img/dog breeds/airedale.png";
+         return "img/dog breeds/airedale.png";
      case iconValue.Akita:
-         return "/img/dog breeds/akitas.png";
+         return "img/dog breeds/akitas.png";
      case iconValue.American_staffordshire:
-         return "/img/dog breeds/american-staffordshire-terrier.png";
+         return "img/dog breeds/american-staffordshire-terrier.png";
      case iconValue.Basset_hound:
-         return "/img/dog breeds/basset-hound.png";
+         return "img/dog breeds/basset-hound.png";
      case iconValue.Beagle:
-         return "/img/dog breeds/beagle.png";
+         return "img/dog breeds/beagle.png";
      case iconValue.Bedlington_terrier:
-         return "/img/dog breeds/bedlington-terrier.png";
+         return "img/dog breeds/bedlington-terrier.png";
      case iconValue.Bernese_mountain:
-         return "/img/dog breeds/bernese-mountain.png";
+         return "img/dog breeds/bernese-mountain.png";
      case iconValue.Bichon_frise:
-         return "/img/dog breeds/bichon-frise.png";
+         return "img/dog breeds/bichon-frise.png";
      case iconValue.Border_collie:
-         return "/img/dog breeds/border-collie.png";
+         return "img/dog breeds/border-collie.png";
      case iconValue.Boxer:
-         return "/img/dog breeds/boxer.png";
+         return "img/dog breeds/boxer.png";
      case iconValue.Bullmastiff:
-         return "/img/dog breeds/bullmastiff.png";
+         return "img/dog breeds/bullmastiff.png";
      case iconValue.Bullterrier:
-         return "/img/dog breeds/bullterrier.png";
+         return "img/dog breeds/bullterrier.png";
      case iconValue.Chinese_crested:
-         return "/img/dog breeds/chinese-crested.png";
+         return "img/dog breeds/chinese-crested.png";
      case iconValue.Chow_chow:
-         return "/img/dog breeds/chow-chow.png";
+         return "img/dog breeds/chow-chow.png";
      case iconValue.Dalmatian:
-         return "/img/dog breeds/dalmatian.png";
+         return "img/dog breeds/dalmatian.png";
      case iconValue.Doberman:
-         return "/img/dog breeds/doberman.png";
+         return "img/dog breeds/doberman.png";
      case iconValue.English_cocker_spaniel:
-         return "/img/dog breeds/english-cocker-spaniel.png";
+         return "img/dog breeds/english-cocker-spaniel.png";
      case iconValue.French_bulldog:
-         return "/img/dog breeds/french-bulldog.png";
+         return "img/dog breeds/french-bulldog.png";
      case iconValue.German_shepherd:
-         return "/img/dog breeds/german-shepherd.png";
+         return "img/dog breeds/german-shepherd.png";
      default:
-         return "img/dog%20breeds/pawprint%20(2).png";
+         return "img/pawprint%20(2).png";
  }
 }
 
